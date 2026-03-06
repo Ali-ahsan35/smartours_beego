a91c3458749f6c61c011f3a1e770395ccc8e3100
Ali-ahsan35 <syedahsan748@gmail.com>
2026-03-06 10:51:47 +0600

    update the api route

--- routers/router.go
+++ routers/router.go
@@ -11,5 +11,5 @@ func init() {
 	beego.Router("/api/breadcrumb", &controllers.RefineController{}, "get:GetBreadcrumb")
 	beego.Router("/api/properties", &controllers.PropertiesController{}, "get:Get")
 	beego.Router("/api/propertydetails", &controllers.PropertyDetailsController{}, "get:Get")
-	beego.Router("/api/categorydetails/:country", &controllers.CategoryDetailsController{}, "get:Get")
+	beego.Router("/api/v1/category/details/:country", &controllers.CategoryDetailsController{}, "get:Get")
 }
